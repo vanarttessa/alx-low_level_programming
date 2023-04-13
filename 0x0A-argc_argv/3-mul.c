cab38c7636c38024f6121830b25f584b28d03e86
vanarttessa <vickanja353@gmail.com>
2023-04-13 04:56:11 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,44 +1,51 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
+ *
  * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int q, w, e, vic, t, digit;
+	int i, d, n, len, f, digit;
 
-	q = 0;
-	w = 0;
-	e = 0;
-	vic = 0;
-	t = 0;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
 	digit = 0;
-	while (s[l]vic != '\0')
-		vic++;
-	while (q < vic && t == 0)
-	{
-		if (s[q] == '-')
-			++w;
 
-		if (s[q] >= '0' && s[q] <= '9')
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[q] - '0';
-			if (w % 2)
+			digit = s[i] - '0';
+			if (d % 2)
 				digit = -digit;
-			e = e * 10 + digit;
-			t = 1;
-			if (s[q + 1] < '0' || s[q + 1] > '9')
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			t = 0;
+			f = 0;
 		}
-	q++;
+		i++;
 	}
-	if (t == 0)
-	return (0);
-	return (e);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
+
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
@@ -51,14 +58,16 @@ int main(int argc, char *argv[])
 	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
-
 	{
 		printf("Error\n");
 		return (1);
 	}
+
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
+
 	printf("%d\n", result);
+
 	return (0);
 }
